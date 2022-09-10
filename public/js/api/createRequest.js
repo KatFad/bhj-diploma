f86e4dc8786e00f91410a9f792d9efefa2ba391d
KatFad <q46q46@mail.ru>
2022-09-10 12:51:27 +0300

    'v1'

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,41 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const createRequest = (opt = {}) => {
+    const xhr = new XMLHttpRequest;
+    let url = opt.url;
+    let formData;
+    if (opt.method === 'GET') {
+        url = new URL(window.location.href.slice(0, -1) + opt.url);
+    
+        if (opt.data) {
+            for (let key in opt.data) {
+                    url.searchParams.set(key, opt.data[key]);
+            };
+        };
+    } else {
+        formData = new FormData;
+        if (opt.data) {
+            for (let key in opt.data) {
+                formData.append(key, opt.data[key]);
+            };
+        };    
+    };    
 
+     try {
+            xhr.open(opt.method, url);
+            xhr.responseType = 'json';
+            xhr.send(formData);
+    }
+    catch (err) {
+            callback(err);
+    }    
+    
+    xhr.addEventListener('load', () => {
+        if (xhr.status === 200) {
+            opt.callback(null, xhr.response);
+        } else {
+            opt.callback(xhr.statustext, null);
+        }
+        }); 
 };
